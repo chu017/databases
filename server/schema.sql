294ad7fccdb674cb3eb12bf764b050c96c57a14a
Leslie Ngo <ngo.leslie@gmail.com>
2021-03-31 21:45:47 -0700

    started creating tables in batch file

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,26 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int(2),
+  userID int(3),
+  messagetext varchar(150),
+  roomID int(3)
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  userID int(3),
+  username varchar(20)
+);
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  roomID int(3),
+  roomname varchar(20)
+
+);
 
 
 /*  Execute this file from the command line by typing:
